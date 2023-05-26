54a71948dac7c91e6881288f81e7dbb2f612d2ed
joshuatauro <joshuatauro45@gmail.com>
2023-05-27 00:19:42 +0530

    day three

---
+++ LinkedLists/01_EASY/reverse.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++>
+using namespace std;
+
+class ListNode{
+    public:
+        int value;
+        ListNode* next;
+
+        ListNode(int value){
+            this->value = value;
+        }
+}
+
+ListNode* reverseList(ListNode* head){
+            if(head == NULL) return head;
+    ListNode* temp = head;
+    ListNode* nextNode=NULL;
+    ListNode* prevNode=NULL;
+    while(temp != NULL){
+        nextNode=temp->next;
+        temp->next = prevNode;
+        prevNode=temp;
+        temp=nextNode;
+    }
+    // temp->next=prevNode;
+    return prevNode;
+    }
+
+int main() {
+
+    return 0;
+}
